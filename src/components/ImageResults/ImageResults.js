a93ac79bfd94d10c978fa8153d9f6661caa54560
Martin <martin.stankard@hotmail.com>
2018-07-07 12:12:26 -0400

    pictures are loading. Onchange to setState is wonky for selctfield compared to the textfield

--- src/components/ImageResults/ImageResults.js
+++ src/components/ImageResults/ImageResults.js
@@ -1,26 +1,46 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types';
-import {
-    GridList,
-    GridTile
-} from 'material-ui/GridList';
-import IconButton from 'material-ui/IconButton'; 
-import ZoomIn from 'material-ui/svg-icons/action/zoom-in';
-import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { GridList, GridTile } from "material-ui/GridList";
+import IconButton from "material-ui/IconButton";
+import ZoomIn from "material-ui/svg-icons/action/zoom-in";
+import Dialog from "material-ui/Dialog";
+import FlatButton from "material-ui/FlatButton";
 
 export default class ImageResults extends Component {
   render() {
-    return (
-      <div>
-        <h1>Image Results</h1>
-      </div>
-    );
-  };
+    let imageListContent;
+    const { images } = this.props;
 
-  
+    if (images) {
+      imageListContent = (
+        <GridList cols={3}>
+          {images.map(img => (
+            <GridTile
+              title={img.tags}
+              key={img.id}
+              subtitle={
+                <span>
+                  by<strong>{img.user}</strong>
+                </span>
+              }
+              actionIcon={
+                <IconButton>
+                  <ZoomIn color="white" />
+                </IconButton>
+              }
+            >
+              <img src={img.largeImageURL} />
+            </GridTile>
+          ))}
+        </GridList>
+      );
+    } else {
+      imageListContent = <div className="loader">Loading...</div>;
+    }
+    return <div>{imageListContent}</div>;
+  }
 }
 
 ImageResults.propTypes = {
-    images: PropTypes.array.isRequired
+  images: PropTypes.array.isRequired
 };
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,78 +1,92 @@
 import React, { Component } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 //components
 import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
-import MenuItem from 'material-ui/MenuItem';
-import ImageResults from '../ImageResults/ImageResults'
-
+import MenuItem from "material-ui/MenuItem";
+import ImageResults from "../ImageResults/ImageResults";
 
 export default class Search extends Component {
-  state = {
-      searchText:'',
-      amount: 5,
-      apiUrl:'https://pixabay.com/api',
-      apiKey:'9490113-5089b9183c6e79d5028cc8e39',
-      images:[]
+  constructor(){
+      super();
+     
+  }
+  
+    state = {
+    searchText: "",
+    amount: 5,
+    apiUrl: "https://pixabay.com/api",
+    apiKey: "9490113-5089b9183c6e79d5028cc8e39",
+    images: [],
+    query: ()=>{
+
+    }
   };
 
   render() {
-      
     return (
       <div>
         <TextField
-            name='searchText'
-            value={this.state.searchText}
-            onChange={this.onTextChange}
-            floatingLabelText='Search For Images'
-            fullWidth={true}
+          name="searchText"
+          value={this.state.searchText}
+          onChange={this.onTextChange}
+          floatingLabelText="Search For Images"
+          fullWidth={true}
         />
-        <br/>
+        <br />
         <SelectField
-        name="amount"
-        floatingLabelText="Amount"
-        value={this.state.amount}
-        onChange={this.onAmountChange}
+          name="amount"
+          floatingLabelText="Amount"
+          value={this.state.amount}
+          onChange={this.onAmountChange}
         >
-            <MenuItem value={5} primaryText="5"/>
-            <MenuItem value={10} primaryText="10"/>
-            <MenuItem value={15} primaryText="15"/>
-            <MenuItem value={30} primaryText="30"/>
-            <MenuItem value={50} primaryText="50"/>
+          <MenuItem value={5} primaryText="5" />
+          <MenuItem value={10} primaryText="10" />
+          <MenuItem value={15} primaryText="15" />
+          <MenuItem value={30} primaryText="30" />
+          <MenuItem value={50} primaryText="50" />
         </SelectField>
-        <br/>
+        <br />
         {this.imageResultConditional()}
       </div>
-     
-    
     );
   }
 
-  onTextChange = (e) => {
-      //generic to the control, with same name in state
-      this.setState({[e.target.name]:e.target.value} );
-      this.queryPixabayApi();
+  onTextChange = e => {
+    //generic to the control, with same name in state
+    this.setState({ [e.target.name]: e.target.value },() => {
+        axios
+          .get(
+            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=
+            ${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+          )
+          .then(res => this.setState({ images: res.data.hits }))
+          .catch(err => console.log(err));
+      });
+   
   };
 
-  onAmountChange = (e , index, value)  =>{ 
-      this.setState({amount:value});
-      this.queryPixabayApi();
-    };
+  onAmountChange = (e, index, value) => {
+    this.setState({ amount: value },() => {
+        axios
+          .get(
+            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=
+            ${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+          )
+          .then(res => this.setState({ images: res.data.hits }))
+          .catch(err => console.log(err));
+      });
 
-  queryPixabayApi =() => {
-    axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
-    .then(res => this.setState({images: res.data.hits}))
-    .catch(err => console.log(err));
+  };
 
-    console.log(this.state.images);
-  }
+ 
 
-  imageResultConditional = () =>{
-      if(this.state.images.length>0){
-        return(<ImageResults images = {this.state.images}/>);
-      }else{
-        return (null);
-      }
-  }
+  imageResultConditional = () => {
+    if (this.state.images.length > 0) {
+      return <ImageResults images={this.state.images} />;
+    } else {
+      return null;
+    }
+  };
 }
